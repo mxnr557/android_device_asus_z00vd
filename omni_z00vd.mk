685980e8454b62e1cbd1c8573a896345c3b3d677
mxnr557 <110708679+mxnr557@users.noreply.github.com>
2022-09-25 03:46:54 +0700

    Update and rename omni_ASUS_Z00VD.mk to omni_z00vd.mk

--- omni_z00vd.mk
+++ omni_z00vd.mk
@@ -11,15 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from ASUS_Z00VD device
-$(call inherit-product, device/asus/ASUS_Z00VD/device.mk)
+$(call inherit-product, device/asus/z00vd/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ASUS_Z00VD
-PRODUCT_NAME := omni_ASUS_Z00VD
+PRODUCT_DEVICE := z00vd
+PRODUCT_NAME := omni_z00vd
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := ASUS_Z00VD
+PRODUCT_MODEL := z00vd
 PRODUCT_MANUFACTURER := asus
